83564c6c10efc84db3694f10e111e04d9f4fe6b5
unknown <martin2844@gmail.com>
2020-03-30 19:27:04 -0300

    env change

--- src/config/base.js
+++ src/config/base.js
@@ -2,17 +2,14 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const app = firebase.initializeApp({
-    apiKey: process.env.FIREBASE_KEY,
-    authDomain: process.env.FIREBASE_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASe_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_SENDER_ID
+    apiKey: process.env.GATSBY_FIREBASE_KEY,
+    authDomain: process.env.GATSBY_FIREBASE_DOMAIN,
+    databaseURL: process.env.GATSBY_FIREBASE_DATABASE,
+    projectId: process.env.GATSBY_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.GATSBY_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.GATSBY_FIREBASE_SENDER_ID
 
 })
 
-console.log(process.env.FIREBASE_DOMAIN, "help2");
-console.log(process.env.NETLIFY);
-console.log(process.env.production.FIREBASE_KEY);
 
 export default app;
