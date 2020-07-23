304bccb5a15eaba299a02b23e609864af1391e82
unknown <martin2844@gmail.com>
2020-07-23 17:04:10 -0300

    mp test

--- src/pages/test.js
+++ src/pages/test.js
@@ -16,13 +16,30 @@ const Test = () => {
         if(process.env.GATSBY_PRODUCTION === "false") {
             axios.post("/api/sendmail/codigomate/", object).then(response => console.log(response));
             // axios.get("/api/sendmail/").then(res => console.log(res));
-            console.log("testing")
+            console.log("testing");
         } else {
-            console.log("produ")
-            axios.post("/heroku/sendmail/codigomate")
+            console.log("produ");
+            axios.post("/heroku/sendmail/codigomate");
         }
        
     } 
+
+    let course = {
+        name: "curso de prueba",
+        price: 200,
+        unit: 1,
+        img: "https://eloquentjavascript.net/img/chapter_picture_1.jpg"
+    }
+
+    const sendTest2 = () => {
+        if(process.env.GATSBY_PRODUCTION === "false") {
+            axios.post("/api/payment/new/", course).then(response => console.log(response));
+        } else {
+            axios.post("http://www.api.codigomate.com/api/payment/new/", course).then(response => console.log(response));
+        }
+       
+    } 
+
 
 
     return (
@@ -30,6 +47,11 @@ const Test = () => {
             <button onClick={sendTest}>
                 Press to send test to api
             </button>
+            <div>
+            <button onClick={sendTest2}>
+                Press to send test to payment api
+            </button>
+            </div>
         </div>
     )
 }
